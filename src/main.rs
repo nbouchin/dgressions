897b9b8107044d1747c648174362adc44a0f181c
root <root@nbouchin.localdomain>
2019-06-11 11:52:57 +0200

    UPDATE(mount.rs) add pop fn

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,29 @@
 mod mount;
 use libc::*;
+use std::process::Command;
 
 fn main() {
-    let mut kfs: mount::KernelFilesystem = mount::KernelFilesystem::new();
+	let mut kfs: mount::KernelFilesystem = mount::KernelFilesystem::new();
 
-    kfs.add(mount::MountInfo::new("udev", "/mnt/dev", "devtmpfs", MS_NOSUID|MS_RELATIME, ""));
-    kfs.add(mount::MountInfo::new("sysfs", "/mnt/sys", "sysfs", MS_NOSUID|MS_NODEV|MS_NOEXEC|MS_RELATIME, ""));
-//    kfs.add(mount::MountInfo::new("var", "/var", "iso9660", 0, ""));// find info on var
-    kfs.add(mount::MountInfo::new("proc", "/mnt/proc", "proc", MS_NOSUID|MS_NODEV|MS_NOEXEC|MS_RELATIME, ""));
-    kfs.add(mount::MountInfo::new("/mnt/dev/vda1", "/mnt/root", "ext4", MS_RDONLY, ""));
+	kfs.add(mount::MountInfo::new("rootfs", "/", "ext4", MS_RDONLY|MS_REMOUNT, ""));
 
-    kfs.mount_kernel_filesystem();
+	kfs.mount_kernel_filesystem();
+	Command::new("fsck")
+		.arg("/dev/sda3")
+		.spawn()
+		.expect("fsck command failed to start");
+
+	kfs.pop();
+
+	kfs.add(mount::MountInfo::new("rootfs", "/", "ext4", MS_REMOUNT, ""));
+	kfs.add(mount::MountInfo::new("udev", "/dev", "devtmpfs", MS_NOSUID|MS_RELATIME, ""));
+	kfs.add(mount::MountInfo::new("sysfs", "/sys", "sysfs", MS_NOSUID|MS_NODEV|MS_NOEXEC|MS_RELATIME, ""));
+	kfs.add(mount::MountInfo::new("proc", "/proc", "proc", MS_NOSUID|MS_NODEV|MS_NOEXEC|MS_RELATIME, ""));
+
+	kfs.mount_kernel_filesystem();
+	Command::new("mount")
+		.spawn()
+		.expect("mount command failed to start");
+	loop {
+	}
 }
--- src/mount.rs
+++ src/mount.rs
@@ -3,56 +3,60 @@ use std::io::{Result, Error};
 use std::collections::linked_list::LinkedList;
 
 pub struct MountInfo {
-    source: &'static str,
-    target: &'static str,
-    file_system_type: &'static str,
-    mount_flags: u64,
-    _data: &'static str,
+source: &'static str,
+		target: &'static str,
+		file_system_type: &'static str,
+		mount_flags: u64,
+		_data: &'static str,
 }
 
 impl MountInfo {
-    pub fn new(source: &'static str, target: &'static str, file_system_type: &'static str, mount_flags: u64, _data: &'static str) -> MountInfo {
-        MountInfo { source, target, file_system_type, mount_flags, _data }
-    }
+	pub fn new(source: &'static str, target: &'static str, file_system_type: &'static str, mount_flags: u64, _data: &'static str) -> MountInfo {
+		MountInfo { source, target, file_system_type, mount_flags, _data }
+	}
 }
 
 pub struct KernelFilesystem {
-    fs_vect: Vec<MountInfo>,
+fs_vect: Vec<MountInfo>,
 }
 
 impl KernelFilesystem {
-    pub fn new() -> KernelFilesystem {
-        KernelFilesystem {
-            fs_vect: Vec::new(),
-        }
-    }
+	pub fn new() -> KernelFilesystem {
+		KernelFilesystem {
+fs_vect: Vec::new(),
+		}
+	}
 
-    pub fn add(&mut self, mi: MountInfo) {
-        self.fs_vect.push(mi);
-    }
+	pub fn add(&mut self, mi: MountInfo) {
+		self.fs_vect.push(mi);
+	}
 
-    pub fn mount_kernel_filesystem(&mut self) {
-        for i in self.fs_vect.iter() {
-           if let Err(e) = mount(i) {
-               println!("{}", e);
-           }
-        }
-    }
+	pub fn pop(&mut self) {
+		self.fs_vect.pop();
+	}
+
+	pub fn mount_kernel_filesystem(&mut self) {
+		for i in self.fs_vect.iter() {
+			if let Err(e) = mount(i) {
+				println!("{}", e);
+			}
+		}
+	}
 }
 
 fn mount(mount: &MountInfo) -> Result<()> {
-    let ret;
+	let ret;
 
-    unsafe {
-        ret = libc::mount(std::ffi::CString::new(mount.source).unwrap().into_raw(),
-        std::ffi::CString::new(mount.target).unwrap().into_raw(),
-        std::ffi::CString::new(mount.file_system_type).unwrap().into_raw(),
-        mount.mount_flags,
-        std::ptr::null_mut())
-    }
+	unsafe {
+		ret = libc::mount(std::ffi::CString::new(mount.source).unwrap().into_raw(),
+				std::ffi::CString::new(mount.target).unwrap().into_raw(),
+				std::ffi::CString::new(mount.file_system_type).unwrap().into_raw(),
+				mount.mount_flags,
+				std::ptr::null_mut())
+	}
 
-    if ret == -1 {
-        return Err(Error::last_os_error());
-    }
-    return Ok(());
+	if ret == -1 {
+		return Err(Error::last_os_error());
+	}
+	return Ok(());
 }
