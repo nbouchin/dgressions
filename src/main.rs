8a6ae8d9f7ab6af91d7c4014285954fdce2f3a6a
Nicolas Bouchinet <nicolas.bouchinet@protonmail.com>
2020-04-28 15:17:53 +0200

    dgressions/task_master: Add start and partial stop command

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,9 @@ use std::collections::HashMap;
 use std::env;
 use std::fs::File;
 use std::io::prelude::*;
-use std::process::{Command, Child};
 use std::path::Path;
+use std::process::{Child, Command};
+use std::{thread, time};
 
 #[derive(Deserialize, Debug)]
 pub struct Config {
@@ -34,7 +35,6 @@ struct Unit {
 }
 
 impl Unit {
-
     pub fn new(path: &'static Path) -> Unit {
         Unit {
             config: Unit::get_config(path).unwrap(),
@@ -47,42 +47,54 @@ impl Unit {
         let mut file = File::open(path.to_str().unwrap_or_default()).expect("Unable to open");
         let mut contents = String::new();
 
-        file.read_to_string(&mut contents).expect("Could not parse config");
+        file.read_to_string(&mut contents)
+            .expect("Could not parse config");
         match toml::from_str::<Config>(&contents) {
             Ok(n) => Ok(n),
             Err(n) => Err(n),
         }
     }
 
-    fn build_env(config: Config) -> HashMap<String, String> {
-        let mut env: HashMap<String, String> = env::vars().collect();
+    fn build_env(config: &Config) -> HashMap<String, String> {
+        let mut env = env::vars().collect::<HashMap<String, String>>();
 
-        for val in config.env.unwrap_or_default().iter() {
+        let conf_env = config.env.as_ref();
+
+        for val in conf_env.unwrap() {
             let (first, last) = val.split_at(val.trim().find('=').unwrap_or(val.len()));
-            env.insert(first.to_string(), last.get(1..).unwrap_or_default().to_string());
+            env.insert(
+                first.to_string(),
+                last.get(1..).unwrap_or_default().to_string(),
+            );
         }
         env
     }
 
-    pub fn run(&self) {
-        let cmd: &String = &self.config.cmd;
-        let args: Vec<String> = self.config.args.unwrap_or_default();
-        let env: HashMap<String, String> = Unit::build_env(self.config);
+    pub fn start(&mut self) {
+        let env = Unit::build_env(&self.config);
+        let args = match &self.config.args {
+            Some(arguments) => arguments.to_owned(),
+            None => vec![],
+        };
 
-        let child = Command::new(cmd)
-            .args(args)
+        let child = Command::new(&self.config.cmd)
+            .args(&args)
             .envs(&env)
             .spawn()
             .expect("Command failed to execute");
 
-        &self.child = Some(child);
+        self.child = Some(child);
+    }
+
+    pub fn stop(&mut self) {
+        self.child.as_mut().unwrap().kill().expect("Command was not running");
     }
 }
 
-
 fn main() -> std::io::Result<()> {
     let path = Path::new("./nginx.service");
-    let unit: Unit = Unit::new(path);
-    unit.run();
+    let mut unit: Unit = Unit::new(path);
+    unit.start();
+    unit.stop();
     Ok(())
 }
